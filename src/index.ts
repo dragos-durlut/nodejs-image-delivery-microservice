6525a63a4cc1687d76fb1664524a6cf958283b88
dragos-durlut <43134494+dragos-durlut@users.noreply.github.com>
2019-12-05 23:06:12 +0200

    - extracted file data - resized file using GM - wrote file using stream and pipe

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import fs from "fs";
 import gm from "gm";
-import stream from "stream"
+import path from "path";
+import stream from "stream";
 const app = express();
 const port = 8080; // default port to listen
 
@@ -15,28 +16,34 @@ app.get("/image/:imageName/:imageResolution", (req, res) => {
     // res.send(`image named ${req.params.imageName} and resolution ${req.params.imageResolution}`);
     const imageName = req.params.imageName;
     const imageResolution = req.params.imageResolution;
-    const fileAbsolutePath = __dirname + `/images/${imageName}`;
+    const imagesFolder = __dirname + `/images/`;
+    const fileAbsolutePath = imagesFolder + `${imageName}`;
     const fileExists = fs.existsSync(fileAbsolutePath);
-    if (fileExists) { //file with requested resolution already exists
-        let resolutionExists: boolean = true; //to see how to organize file and check for different resolutions
+    if (fileExists) { // file with requested resolution already exists
+        const resolutionExists: boolean = false; // to see how to organize file and check for different resolutions
         if (resolutionExists) {
-            //res.sendFile(fileAbsolutePath);
+            // res.sendFile(fileAbsolutePath);
             // If the image does exist on the file system, then stream the image to the response object
             fs.createReadStream(fileAbsolutePath)
                 .pipe(res);
-        }
-        else { //file with required resolution does not exist, we must resize it
-            gm(fileAbsolutePath).resize(100, 100, null);
+        } else { // file with required resolution does not exist, we must resize it
+            const fileExtension = path.extname(fileAbsolutePath);
+            const fileName = path.basename(fileAbsolutePath).replace(fileExtension, "");
+            const resizedFileName = `${fileName}_100x100${fileExtension}`;
+            const resizeFileAbsolutePath = imagesFolder + `${resizedFileName}`;
+            // create a write stream of the resized image to the system// we have ti give a new name
+            const resizedFileStreamPassThrough = gm(fileAbsolutePath).resize(100, 100, null).stream();
+            resizedFileStreamPassThrough.pipe(fs.createWriteStream(resizeFileAbsolutePath));
 
             // Create a write stream to the file system
-            req.pipe(
+            /*req.pipe(
                 new stream.PassThrough().pipe(
                     fs.createWriteStream(fileAbsolutePath)
                 )
-            );
+            );*/
 
             // pipe to the response at the same time
-            req.pipe(res);
+            resizedFileStreamPassThrough.pipe(res);
         }
     } else {
         res.status(404).send({ error: `image named ${imageName} and resolution ${imageResolution} was not found` });
