829c4c744056012605c51f342e7bb0ea6baa03da
dragos-durlut <43134494+dragos-durlut@users.noreply.github.com>
2019-12-06 19:22:41 +0200

    - removed GM - removed imagemagick - installed sharp - resied image with sharp

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import fs from "fs";
-import gm from "gm";
 import path from "path";
+import sharp from "sharp";
 import stream from "stream";
 const app = express();
 const port = 8080; // default port to listen
@@ -32,8 +32,26 @@ app.get("/image/:imageName/:imageResolution", (req, res) => {
             const resizedFileName = `${fileName}_100x100${fileExtension}`;
             const resizeFileAbsolutePath = imagesFolder + `${resizedFileName}`;
             // create a write stream of the resized image to the system// we have ti give a new name
-            const resizedFileStreamPassThrough = gm(fileAbsolutePath).resize(100, 100, null).stream();
-            resizedFileStreamPassThrough.pipe(fs.createWriteStream(resizeFileAbsolutePath));
+            /*gm(fileAbsolutePath).resize(100, 100).stream((err, stdout, stderr) => {
+                // https://stackoverflow.com/a/12665226/249895
+                // https://stackoverflow.com/questions/30913540/why-fs-createreadstream-only-pipe-one-time
+                // https://stackoverflow.com/questions/12468471/nodejs-gm-resize-and-pipe-to-response
+                // https://stackoverflow.com/questions/25372402/graphicsmagick-processes-resulting-in-empty-file
+                // stdout.pipe(res);
+                const writeStream = fs.createWriteStream(resizeFileAbsolutePath);
+                stdout.pipe(writeStream); // pipe to response
+            });*/
+            sharp(fileAbsolutePath).resize(100, 100).toFile(resizeFileAbsolutePath, (err, info) => {
+                if (err) {
+                    // tslint:disable-next-line:no-console
+                    console.error(err);
+                    res.status(404).send({ error: err });
+                } else {
+                    res.status(200).sendFile(resizeFileAbsolutePath);
+                }
+            });
+
+            // resizedFileStreamPassThrough.pipe(fs.createWriteStream(resizeFileAbsolutePath));
 
             // Create a write stream to the file system
             /*req.pipe(
@@ -43,7 +61,7 @@ app.get("/image/:imageName/:imageResolution", (req, res) => {
             );*/
 
             // pipe to the response at the same time
-            resizedFileStreamPassThrough.pipe(res);
+            // resizedFileStreamPassThrough.pipe(res);
         }
     } else {
         res.status(404).send({ error: `image named ${imageName} and resolution ${imageResolution} was not found` });
