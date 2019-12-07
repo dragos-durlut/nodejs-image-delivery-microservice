efb83024d8ef9b07330cc4f55536c72f83290166
dragos-durlut <43134494+dragos-durlut@users.noreply.github.com>
2019-12-07 11:07:24 +0200

    - added isSameAsOriginalImage to ResizedServedImage - added AreResolutionsEqual() to ServedImageResolution

--- src/models/resized-served-image.ts
+++ src/models/resized-served-image.ts
@@ -1,5 +1,6 @@
 import { ServedImage } from "./served-image";
 
 export class ResizedServedImage extends ServedImage {
+    public isSameAsOriginalImage: boolean = false;
     public originalImage: ServedImage;
 }
--- src/models/served-image-resolution.ts
+++ src/models/served-image-resolution.ts
@@ -1,4 +1,9 @@
 export class ServedImageResolution {
+
+    public static AreResolutionsEqual(resolutionA: ServedImageResolution, resolutionB: ServedImageResolution): boolean {
+        return resolutionA.height === resolutionB.height && resolutionA.width === resolutionB.width;
+    }
     public width: number;
     public height: number;
+
 }
