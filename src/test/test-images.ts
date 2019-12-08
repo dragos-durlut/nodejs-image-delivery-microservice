786b39e6c83849e5371f7966e02cd7e7de6dda88
dragos-durlut <43134494+dragos-durlut@users.noreply.github.com>
2019-12-08 16:12:13 +0200

    - added validation of the image validator

--- src/test/test-images.ts
+++ src/test/test-images.ts
@@ -1,9 +1,28 @@
-import { expect } from "chai";
+import "mocha";
 import request from "request";
+import { expect} from "chai";
 
-it("Main page content", (done) => {
-    request("http://localhost:8080", (error, response, body) => {
-        expect(body).to.equal("Hello World");
+import { RequestImageValidatorService } from "../services/request-image-validator-service";
+
+
+describe('Image validator validator', () => {
+
+    it('should pass validation', (done) => {
+
+        let validatorService: RequestImageValidatorService = new RequestImageValidatorService("img1.png", "300x600")
+        let isValid: boolean = validatorService.validateImage();
+        expect(isValid).to.be.true;
+        expect(validatorService.errors.length).to.to.equal(0);
         done();
     });
+
+    it('should not pass validation', (done) => {
+
+        let validatorService: RequestImageValidatorService = new RequestImageValidatorService("img1.XLSX", "ab0x600")
+        let isValid: boolean = validatorService.validateImage();
+        expect(isValid).to.be.false;
+        expect(validatorService.errors.length).to.to.equal(2);
+        done();
+    });
+
 });
